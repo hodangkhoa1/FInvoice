806af29c7159859d9f7d09482dce83e8d04fb133
hodangkhoa1 <hodangkhoa110702@gmail.com>
2023-03-22 23:13:04 +0700

    Update code

---
+++ BackEnd/FinvoiceWeb/wwwroot/js/Admin/AdminRoot.js
@@ -0,0 +1,16 @@
+﻿let menuButton = document.querySelector(".menu-button");
+let navigation = document.querySelector(".navigation");
+let bodyMain = document.querySelector(".body-main");
+let menuOpen = true;
+
+menuButton.onclick = function () {
+    navigation.classList.toggle("active");
+    bodyMain.classList.toggle("active");
+    if (menuOpen) {
+        menuButton.classList.toggle("open");
+        menuOpen = false;
+    } else {
+        menuButton.classList.toggle("open");
+        menuOpen = true;
+    }
+};
